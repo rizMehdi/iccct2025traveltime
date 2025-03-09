1d144c4bb351fcb7079c54e6b4c5541bc4f52013
rizMehdi <ccmeall@gmail.com>
2025-03-09 21:06:50 +0000

    draw in mid

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ chart_distances = alt.Chart(df_team_distances).mark_bar().encode(
 chart_results = alt.Chart(df_team_results).mark_bar().encode(
     x='Count:Q',
     y=alt.Y('Team:N', sort=team_order),  # Use the specified team order
-    color=alt.Color('Result:N', scale=alt.Scale(domain=['Wins', 'Losses', 'Draws'], range=['green', 'red', 'blue']))
+    color=alt.Color('Result:N', scale=alt.Scale(domain=['Wins',  'Draws', 'Losses'], range=['green', 'blue', 'red']))
 ).properties(
     title='Match Results per Team'
 )

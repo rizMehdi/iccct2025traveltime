6657ff4b50bd4573587a55a86c1d5d8d67358b97
rizMehdi <ccmeall@gmail.com>
2025-03-09 15:19:02 +0000

    p07

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ def add_plane_line(start, end, color, offset=0):
 offset = 0
 for start, end, color in travel_routes:
     add_plane_line(start, end, color, offset)
-    offset += 0.05  # Increment offset for parallel lines
+    offset += 0.07  # Increment offset for parallel lines
     # offset += 0.01  # Increment offset for parallel lines
 
 # Display the map
